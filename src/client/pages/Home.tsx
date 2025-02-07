9b8e3c85ae9592bc37a085ed94182ab46e293417
prominhaj <parsonal322532@gmail.com>
2025-02-07 23:23:40 +0600

    Implement Tailwindcss

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 const Home = () => {
     return (
         <div>
-            Home Page
+            <h2 className='font-semibold text-2xl'>Home Page</h2>
             <Link to="/dashboard">Dashboard</Link>
         </div>
     );
