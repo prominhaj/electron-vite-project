4f13dcd951adf36e3ba6dc6b07a4fbb549b72b23
prominhaj <parsonal322532@gmail.com>
2025-02-07 15:21:57 +0600

    Update Vite variable Declare in global

---
+++ global.d.ts
@@ -0,0 +1,3 @@
+// Declare the type for the `MAIN_WINDOW_VITE_DEV_SERVER_URL` global variable
+declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string | undefined;
+declare const MAIN_WINDOW_VITE_NAME: string | undefined;
